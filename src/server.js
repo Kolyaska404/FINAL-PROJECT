9b10f9d23d346ad2809c1979fedf3d52724c106e
Nikolay Didenko <147000338+Kolyaska404@users.noreply.github.com>
2024-12-13 23:51:01 +0300

    Repair backend

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express from "express";
+import mysql from 'mysql'
+import bodyParser from "body-parser";
+import cors from 'cors'
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const PORT = 4200;
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -22,33 +24,17 @@ db.connect((err) => {
 })
 
 app.post('/submit', (req, res) => {
-    const {
-        service,
-        kind,
-        goal,
-        pages,
-        functional,
-        budget,
-        material,
-        url,
-        yes_no,
-        username,
-        marketName,
-        phone
+    const formData = req.body;
+    const sql = 'INSERT INTO forms SET ?';
     
-    } = req.body
-    const sql = `INSERT INTO submissions (service, type, goal, pages, functionality, budget, material, url, name, store_name, phone) VALUES (?,?,?,?,?,?,?,?,?,?,?)`
-    const values = [service, kind, goal, pages, functional, budget, material, url, yes_no, username, marketName, phone]
-    
-    db.query(sql, values, (err, result) => {
+    db.query(sql, formData, (err, result) => {
         if (err) {
             return res.status(500).json({ error: err.message});
         }
-        res.status(200).json({ message: 'Submission successfull!'});
+        res.status(201).json({ id: result.insertId, message: 'Form submitted successfully' });
     });
 });
 
-const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
